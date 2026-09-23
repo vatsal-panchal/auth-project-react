6d8afcddb583379bdbe614a7d99ad5c2558ddbf0
vatsal-panchal <panchalvatsal2025@gmail.com>
2026-09-23 09:24:01 +0530

    code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppRoutes from "./routes/AppRoutes.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(<AppRoutes />);
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AuthLayout from "../layouts/AuthLayout";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import MainLayout from "../layouts/MainLayout";
+
+const AppRoutes = () => {
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AuthLayout />,
+      children: [
+        {
+          path: "",
+          element: <LoginPage />,
+        },
+        {
+          path: "register",
+          element: <RegisterPage />,
+        },
+      ],
+    },
+    {
+      path: "/main",
+      element: <MainLayout />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default AppRoutes;
